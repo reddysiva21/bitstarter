a541b1cbb42a3d9fba1fa9674b3f4a3a152f6bcd
reddysiva21 <reddysiva21@gmail.com>
2013-07-18 13:35:53 +0000

    update2

--- web.js
+++ web.js
@@ -1,10 +1,10 @@
 var express = require('express');
 
 var app = express.createServer(express.logger());
-var buf = new Buffer(fs.readFileSync("index.html"),"utf-8");
+var buf = new Buffer(fs.readFileSync('./index.html'),'utf-8');
 
 app.get('/', function(request, response) {
-  response.send(buf.toString('utf-8', 0, buf.length));
+  response.send(buf.toString());
 });
 
 var port = process.env.PORT || 5000;
