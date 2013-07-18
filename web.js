145bb4df82f0de71964a98f4b07ccf38c0e91fed
reddysiva21 <reddysiva21@gmail.com>
2013-07-18 13:30:00 +0000

    update

--- web.js
+++ web.js
@@ -1,7 +1,7 @@
 var express = require('express');
 
 var app = express.createServer(express.logger());
-var buf = fs.readFileSync("index.html");
+var buf = new Buffer(fs.readFileSync("index.html"),"utf-8");
 
 app.get('/', function(request, response) {
   response.send(buf.toString('utf-8', 0, buf.length));
